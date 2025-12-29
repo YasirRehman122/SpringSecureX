727cd3c38bf5b321d740bbcebf1b2c0a41365290
Yasir Rehman <engnryasir122@gmail.com>
2025-12-30 02:02:24 +0500

    Initial commit: SpringSecureX project setup

---
+++ src/main/java/com/springsecurex/spring_securex/SpringSecureXApplication.java
@@ -0,0 +1,13 @@
+package com.springsecurex.spring_securex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecureXApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecureXApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springsecurex/spring_securex/SpringSecureXApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springsecurex.spring_securex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecureXApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
